65278373aeb3e29b26c2ff85dde232739a51bae7
sgraziola <simo.fdl@gmail.com>
2022-11-16 19:07:37 +0100

    Boolflix: Add flags (bad practice)

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,9 +1,11 @@
 <script>
 export default {
     name: 'MoviesItem',
+
     props: {
         movie: Object
-    }
+    },
+
 
 }
 </script>
@@ -14,7 +16,16 @@ export default {
             <ol>
                 <li>Titolo: {{ movie.title }}</li>
                 <li>Titolo Originale: {{ movie.original_title }}</li>
-                <li>Lingua: {{ movie.original_language }}</li>
+                <li>
+                    Lingua:
+                    <span v-if="movie.original_language == 'en'"><img src="../assets/img/en.png" alt=""></span>
+                    <span v-if="movie.original_language == 'it'"><img src="../assets/img/it.png" alt=""></span>
+                    <span v-if="movie.original_language == 'fr'"><img src="../assets/img/fr.png" alt=""></span>
+                    <span v-if="movie.original_language == 'de'"><img src="../assets/img/de.png" alt=""></span>
+                    <span v-if="movie.original_language == 'es'"><img src="../assets/img/es.png" alt=""></span>
+                    <span v-else>{{ movie.original_language }}</span>
+
+                </li>
                 <li>Voto: {{ movie.vote_average }}</li>
             </ol>
         </div>
